9181b1b128d3965215e37fc8c6f95cdc7c7d1a8a
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-07-20 07:43:55 -0500

    feat: creating footer and sidebar components

--- src/app/user/components/layout/layout.component.ts
+++ src/app/user/components/layout/layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, SidebarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
