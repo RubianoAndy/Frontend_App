c437edff533715f005aebcf48cf26bcc0efa7980
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-07-20 12:51:45 -0500

    frat: adding auth component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthModule } from './user/components/auth/auth.module';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./user/components/layout/layout.component') },
+
+    { path: 'auth', loadChildren: () => import('./user/components/auth/auth.module').then((m) => m.AuthModule) },
+
+    // { path: 'login', loadComponent: () => import('./user/components/authentication/login/login.component') },
 ];
