d50fdbb4c81331803b34c0c4872d82b71f999092
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-07-20 17:45:37 -0500

    feat: auth responsive structure

--- src/app/user/components/auth/login/login.component.ts
+++ src/app/user/components/auth/login/login.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+    // AngularSvgIconModule,
+    NgClass,
+    NgIf,
+    // ButtonComponent
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  submitted = false;
+  passwordTextType!: boolean;
 
+  constructor(
+    private readonly _formBuilder: FormBuilder,
+    private readonly _router: Router
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(data: any = null) {
+    data = data || {
+      email: '',
+      password: '',
+    }
+
+    this.form = this._formBuilder.group({
+      email: [data.email, [Validators.required, Validators.email]],
+      password: [data.password, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    const { email, password } = this.form.value;
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    this._router.navigate(['/']);
+  }
 }
