431909df42a6809e5657003410cfe5b5c23f77ee
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-07-20 07:38:31 -0500

    feat: creating layout and header components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadComponent: () => import('./user/layout/layout.component') },
+    { path: '', loadComponent: () => import('./user/components/layout/layout.component') },
 ];
--- src/app/user/components/layout/layout.component.ts
+++ src/app/user/components/layout/layout.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [
-    RouterOutlet
-  ],
+  imports: [],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
