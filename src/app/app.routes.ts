b84819e4f12af83a1e65cad6d99d2d20de4aa2bb
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-07-21 13:02:41 -0500

    feat: adding reditectTo main rout in Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,5 @@ export const routes: Routes = [
     { path: '', loadComponent: () => import('./user/components/layout/layout.component') },
     { path: 'auth', loadChildren: () => import('./user/components/auth/auth.module').then((m) => m.AuthModule) },
     { path: 'dashboard', loadComponent: () => import('./user/components/dashboard/dashboard.component'), canActivate: [authGuard] },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
