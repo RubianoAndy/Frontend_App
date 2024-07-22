356a7b9f631140a95b0a2db073447302c8c9237d
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-07-22 18:22:59 -0500

    feat: delete unnecesary components

--- src/app/user/components/layout/layout.component.ts
+++ src/app/user/components/layout/layout.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 
 import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, SidebarComponent],
+  imports: [],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
