ee9f2705447e63051857dea4cf2c0b4c16523c9b
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-07-22 21:24:31 -0500

    feat: sidebar moving

--- src/app/user/components/dashboard/dashboard.component.ts
+++ src/app/user/components/dashboard/dashboard.component.ts
@@ -1,18 +1,24 @@
 import { Component, ElementRef, Renderer2, ViewChild  } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router, RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NgClass,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
   @ViewChild('dropdownMenu', { static: true }) dropdownMenu!: ElementRef;
+  @ViewChild('sidebar', { static: false }) sidebar!: ElementRef;
+
+  isSidebarOpen = true;
+
   loading: boolean = false;
 
   constructor (
@@ -34,6 +40,23 @@ export class DashboardComponent {
       this.renderer.addClass(dropdown, 'hidden');
       this.renderer.setAttribute(dropdown, 'aria-expanded', 'false');
     }
+  }
+
+  // toggleSidebar() {
+  //   const sidebar = this.sidebar.nativeElement;
+  //   const isHidden = sidebar.classList.contains('-translate-x-full');
+    
+  //   if (isHidden) {
+  //     sidebar.classList.remove('-translate-x-full');
+  //     sidebar.classList.add('translate-x-0');
+  //   } else {
+  //     sidebar.classList.add('-translate-x-full');
+  //     sidebar.classList.remove('translate-x-0');
+  //   }
+  // }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 
   logout() {
