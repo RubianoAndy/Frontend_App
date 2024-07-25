7ab97f6fbaa0b0c6206a828869dfe087244605db
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-07-25 11:23:24 -0500

    feat: adding form to code 6 digits in change password

--- src/app/user/components/auth/change-password/change-password.component.ts
+++ src/app/user/components/auth/change-password/change-password.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 import { TranslatePipe } from '../../../../../translate/translate.pipe';
+
 
 @Component({
   selector: 'app-change-password',
   standalone: true,
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+    NgClass,
+    NgIf,
     TranslatePipe
   ],
   templateUrl: './change-password.component.html',
@@ -13,9 +22,42 @@ import { TranslatePipe } from '../../../../../translate/translate.pipe';
 export default class ChangePasswordComponent implements OnInit{
   language: 'en' | 'es' = 'es';
 
-  constructor () {}
+  loading: boolean = false;
+
+  form!: FormGroup;
+
+  isPasswordVisible: boolean = false;
+
+  constructor (
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
-    
+    this.createForm();
+  }
+
+  createForm(data: any = null) {
+    data = data || {
+      code_1: null,
+      code_2: null,
+      code_3: null,
+      code_4: null,
+      code_5: null,
+      code_6: null,
+
+      // password: '',
+    }
+
+    this.form = this.formBuilder.group({
+      code_1: [data.code_1, [ Validators.required ]],
+      code_2: [data.code_2, [ Validators.required ]],
+      code_3: [data.code_3, [ Validators.required ]],
+      code_4: [data.code_4, [ Validators.required ]],
+      code_5: [data.code_5, [ Validators.required ]],
+      code_6: [data.code_6, [ Validators.required ]],
+      
+      // password: [data.password, [ Validators.required, Validators.minLength(6), Validators.maxLength(20) ]],
+    });
   }
 }
