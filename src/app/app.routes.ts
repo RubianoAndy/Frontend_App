6d1b74932707c21abef277b9b0f14bc58e1b26db
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-08-02 17:51:55 -0500

    fix: selected menu in navbar bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { authGuard } from './user/guards/auth/auth.guard';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./web/components/homepage/homepage.component') },
+    { path: 'about', loadComponent: () => import('./web/components/about/about.component') },
     { path: 'contact', loadComponent: () => import('./web/components/contact/contact.component') },
     { path: 'auth', loadChildren: () => import('./user/components/auth/auth.routes').then((m) => m.AuthRoutes) },
     { path: 'user', loadChildren: () => import('./user/components/dashboard/dashboard.routes').then((m) => m.DashboardRoutes), canActivate: [authGuard] },
