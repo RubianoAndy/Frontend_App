aff7b0302a5b36b8b7e12c546725ec0e2897942e
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-08-02 18:32:38 -0500

    feat: adding global services in shared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    NavbarComponent,
     RouterOutlet
   ],
   templateUrl: './app.component.html',
