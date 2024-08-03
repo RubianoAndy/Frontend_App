a10abeb9bfa169f5bd1b6e8db96ad07dfeb1357d
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-08-02 21:58:58 -0500

    fix: delete unnecessary imports in banner

--- src/app/global/components/banner/banner.component.ts
+++ src/app/global/components/banner/banner.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
-import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [
-    NgClass,
-    NgFor,
-    NgIf,
-    NgStyle
-  ],
+  imports: [],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
