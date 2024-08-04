1d0688bbc0288f1e7cc26b75a1b23fc33ebb3400
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-08-03 21:39:35 -0500

    feat: create and add legal components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ export const routes: Routes = [
     { path: 'about', loadComponent: () => import('./web/components/about/about.component') },
     { path: 'contact', loadComponent: () => import('./web/components/contact/contact.component') },
     { path: 'faq', loadComponent: () => import('./web/components/faq/faq.component') },
+    { path: 'terms-and-conditions', loadComponent: () => import('./web/components/terms-and-conditions/terms-and-conditions.component') },
+    { path: 'privacy-policy', loadComponent: () => import('./web/components/privacy-policy/privacy-policy.component') },
     { path: 'auth', loadChildren: () => import('./user/components/auth/auth.routes').then((m) => m.AuthRoutes) },
     { path: 'user', loadChildren: () => import('./user/components/dashboard/dashboard.routes').then((m) => m.DashboardRoutes), canActivate: [authGuard] },
     { path: '**', redirectTo: '', pathMatch: 'full' },
