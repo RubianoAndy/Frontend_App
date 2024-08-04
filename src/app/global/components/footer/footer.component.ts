bacec35836d18d3939f970af39bacd303c13283f
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-08-03 19:53:12 -0500

    feat: adding features component

--- src/app/global/components/footer/footer.component.ts
+++ src/app/global/components/footer/footer.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { TranslatePipe } from '../../../../translate/translate.pipe';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslatePipe
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  language: 'en' | 'es' = 'es';
 
+  page = environment.site_name;
+  currentYear = environment.currentYear;
 }
--- src/app/web/components/homepage/homepage.component.ts
+++ src/app/web/components/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../global/components/navbar/navbar.component';
 import { BannerComponent } from '../../../global/components/banner/banner.component';
+import { FeaturesComponent } from '../../../global/components/features/features.component';
 import { FooterComponent } from '../../../global/components/footer/footer.component';
 
 import { TranslatePipe } from '../../../../translate/translate.pipe';
@@ -11,6 +12,7 @@ import { TranslatePipe } from '../../../../translate/translate.pipe';
   imports: [
     NavbarComponent,
     BannerComponent,
+    FeaturesComponent,
     FooterComponent,
     TranslatePipe,
   ],
