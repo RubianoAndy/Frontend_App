02e5992777be5ff5b171db4602d966fb02428fce
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-08-05 12:06:28 -0500

    fix: modify cards in about

--- src/app/web/components/about/about.component.ts
+++ src/app/web/components/about/about.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../global/components/navbar/navbar.component';
 import { BannerComponent } from '../../../global/components/banner/banner.component';
+import { FooterComponent } from '../../../global/components/footer/footer.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
     NavbarComponent,
+    FooterComponent,
     BannerComponent,
   ],
   templateUrl: './about.component.html',
