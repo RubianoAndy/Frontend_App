a3a6dc801f7119b76707af1c9a3b532664aedbd8
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-08-07 18:26:10 -0500

    feat: update logo in footer

--- src/app/global/components/footer/footer.component.ts
+++ src/app/global/components/footer/footer.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { environment } from '../../utils/environments/environment';
+import { Subscription } from 'rxjs';
+
 import { TranslateService } from '../../services/translate/translate.service';
 
 
@@ -13,14 +15,31 @@ import { TranslateService } from '../../services/translate/translate.service';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
+  logo = environment.logo;
   page = environment.site_name;
   currentYear = environment.currentYear;
+
+  private languageSubscription: Subscription | undefined;
 
   constructor (
     private translateService: TranslateService,
   ) {
 
+  }
+
+  ngOnInit(): void {
+    this.languageSubscription = this.translateService.currentLanguage$.subscribe(language => {
+      if (language === 'pt')
+        this.logo = environment.logo_brazil;
+      else
+        this.logo = environment.logo;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.languageSubscription)
+      this.languageSubscription.unsubscribe();
   }
 
   getTranslation(key: string): string {
